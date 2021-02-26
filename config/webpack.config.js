c4529299c13ae18fcd94dd79aa459cf1d4543f5f
lipk <mail@lipk.org>
2021-02-26 18:15:05 +0800

    更新 webpack 打包输出路径

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/main.ts",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
     filename: "build.js"
   },
   resolve: {
